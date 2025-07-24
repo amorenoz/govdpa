f61682bdd14edd7b9ecc07fc99cafb7d8041b837
Adrian Moreno <amorenoz@redhat.com>
2025-07-24 16:49:20 +0200

    vduse: get vdpa device from vduse
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- pkg/kvdpa/vduse.go
+++ pkg/kvdpa/vduse.go
@@ -29,6 +29,7 @@ const (
 // VduseDevice contains information about a VDUSE Device
 type VduseDevice interface {
 	Name() string
+	VdpaDevice() (VdpaDevice, error)
 }
 
 // vduseDev implements VduseDevice interface
@@ -39,6 +40,28 @@ type vduseDev struct {
 // Name returns the Vduse device's name
 func (vu *vduseDev) Name() string {
 	return vu.name
+}
+
+// VdpaDevice returns the vdpa associated with a vduse device if any
+func (vu *vduseDev) VdpaDevice() (VdpaDevice, error) {
+	devices, err := listVdpaDevicesWithBusDevName("", "vduse")
+	if err != nil {
+		return nil, err
+	}
+	for _, dev := range devices {
+		// A more reliable way would be to check if /dev/{vu.Name} has
+		// the same major/minor number as {dev}.ParentDevicePath()/device.
+		// But golang Stat is unable to return device information. Looking
+		// at the name seems good enough.
+		parent, err := dev.ParentDevicePath()
+		if err != nil {
+			return nil, err
+		}
+		if filepath.Base(parent) == vu.name {
+			return dev, nil
+		}
+	}
+	return nil, nil
 }
 
 /*ListVduseDevices returns a list of all available VDUSE devices */
@@ -241,4 +264,15 @@ func DestroyVduseDevice(name string) error {
 		return fmt.Errorf("ioctl VDUSE_DESTROY_DEV failed: %s", errno.Error())
 	}
 	return nil
+}
+
+// GetVduseDevice returns the vduse device with a given name
+func GetVduseDevice(name string) (VduseDevice, error) {
+	file := filepath.Join(vduseDevDir, name)
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return nil, fmt.Errorf("vduse device %s does not exist", name)
+	}
+	return &vduseDev{
+		name: name,
+	}, nil
 }
